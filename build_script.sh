eb089179aac2a8aa9bbd353ba2186ce164ade2d1
The-Kharsair-Empire <cjy2951042633@gmail.com>
2023-03-11 23:10:51 +1100

    validate build script

--- build_script.sh
+++ build_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash/
 subdircount=`find -L $(pwd)/external/ -mindepth 1 -maxdepth 1 -type d | wc -l`
-[ $subdircount -gt 0 ] || (echo "external dependency not cloned properly, cloning submodules" && git submodule update --recursive)
+[ $subdircount -gt 0 ] || (echo "external dependency not cloned properly, cloning submodules" && git submodule update --recursive --init)
 
 echo "installing dependencies"
-package=( "libx11-dev" "libgl-dev" "libpng-dev", "gnuplot")
+package=( "libx11-dev" "libgl-dev" "libpng-dev" "gnuplot" "cmake" "g++")
 
 for p in "${package[@]}"; do
     if [ $(dpkg-query -W -f='${Status}' $p 2>/dev/null | grep -c "ok installed") -eq 0 ];
